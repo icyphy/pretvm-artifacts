ae940d4f632ec9ac26f481817c9a520a6195299c
erlingrj <erling.jellum@gmail.com>
2024-11-08 15:42:16 -0800

    Delete mistakenly added files

--- copy_gen_graphs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp satellite-controller/src-gen/SatelliteAttitudeController/graphs/dag_raw_frag_1.dot ../../../Documents/papers/pretvm-rtas/images/satellite_dag.dot
-cp satellite-controller/src-gen/SatelliteAttitudeController/graphs/dag_partitioned_frag_1.dot ../../../Documents/papers/pretvm-rtas/images/satellite_partitioned.dot
