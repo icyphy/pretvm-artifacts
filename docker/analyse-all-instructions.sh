b7afe7642e59a8cfa40c69256d2b233b3db028c2
erlingrj <erling.jellum@gmail.com>
2024-11-08 13:37:12 -0800

    Have analyze script produce nsec instead of cycles

--- docker/analyse-all-instructions.sh
+++ docker/analyse-all-instructions.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+result_file="result_pretvm_instructions.txt"
 
 make build-patmos file=pretvm-instructions/inst_lib
 
-make list-symbols file=pretvm-instructions/inst_lib | while read -r line; do make analysis file=pretvm-instructions/inst_lib symbol="$line"; done 2> stderr.log
+make list-symbols file=pretvm-instructions/inst_lib \
+| while read -r line; do make analysis file=pretvm-instructions/inst_lib symbol="$line"; done 2> stderr.log \
+| grep -e "  cycles: " -e "analysis-entry:" >> $result_file
+
+
+./cycles_to_nsec.sh $result_file
